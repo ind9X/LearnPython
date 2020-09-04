68ef83c860c0292105b2cb5d8e5d639c59af9292
ind9X <1371544781@qq.com>
2020-09-04 16:27:59 +0800

    20200904

--- list.py
+++ list.py
@@ -15,14 +15,16 @@ print(x)
 #5.3
 import sys
 
-team1txt = input("请输入第一支队伍：")
-team2txt = input("请输入第二支队伍：")
+team1txt = input("请输入第一支队伍：").strip()
+while team1txt == "":
+    team1txt = input("请正确输入第一支队伍名称：").strip()
+team2txt = input("请输入第二支队伍：").strip()
+while team2txt == "" or team1txt == team2txt:
+    team2txt = input("请正确输入第二支队伍名称：").strip()
 team1 = []
 team2 = []
 
-team_score()
-
-def team_score():
+def main():
     team = input("请选择给哪支队伍进行加分：1.%s     2.%s     （输入g出结果，输入q退出）" % (team1txt,team2txt))
     while team == "1":
         score = input("请输入%s得分（输入q返回上一层）："% team1txt)
@@ -44,11 +46,11 @@ def team_score():
         sys.exit(0)
     else:
         print("输入的选项有误，请重新选择！")
-        team_score()
+        main()
 
 def inputscore(score,team):
     if score == "q":
-        team_score()
+        main()
     else:
         try:
             score = int(score)
@@ -60,3 +62,5 @@ def inputscore(score,team):
                 team1.append(score)
             else:
                 team2.append(score)
+
+main()
